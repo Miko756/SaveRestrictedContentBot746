f302ea50419c8d782fa2d59c70560a162c7afe88
X-STaR / <117068254+ItZxSTaR@users.noreply.github.com>
2022-11-11 14:07:36 +0530

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,2 @@
+echo "starting Bot ~@TheAltron";
+python3 -m main
---
+++ main/plugins/start.py
@@ -0,0 +1,41 @@
+import os
+from .. import bot as Altron
+from telethon import events
+
+from ethon.mystarts import start_srb
+
+@Altron.on(events.callbackquery.CallbackQuery(data="set"))
+async def sett(event):    
+    AltronX = event.client
+    await event.delete()
+    async with AltronX.conversation(event.chat_id) as conv: 
+        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
+        x = await conv.get_reply()
+        if not x.media:
+            xx.edit("No media found.")
+        mime = x.file.mime_type
+        if not 'png' in mime:
+            if not 'jpg' in mime:
+                if not 'jpeg' in mime:
+                    return await xx.edit("No image found.")
+        await xx.delete()
+        t = await event.client.send_message(event.chat_id, 'Trying.')
+        path = await event.client.download_media(x.media)
+        if os.path.exists(f'{event.sender_id}.jpg'):
+            os.remove(f'{event.sender_id}.jpg')
+        os.rename(path, f'./{event.sender_id}.jpg')
+        await t.edit("Temporary thumbnail saved!")
+        
+@Altron.on(events.callbackquery.CallbackQuery(data="rem"))
+async def remt(event):
+    await event.edit('Trying.')
+    try:
+        os.remove(f'{event.sender_id}.jpg')
+        await event.edit('Removed!')
+    except Exception:
+        await event.edit("No thumbnail saved.")                        
+  
+@Altron.on(events.NewMessage(incoming=True, pattern="/start"))
+async def start(event):
+    text = "Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @TheAltron"
+    await start_srb(event, text)
